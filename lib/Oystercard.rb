60652aba38bd5d17c12c858176c1a3a8a1879ac4
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-09-29 17:23:13 +0100

    further refactoring and adding double tap_in / tap_out failsafes

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,12 +1,12 @@
 class Oystercard
-  attr_accessor :balance , :in_use , :entry_station , :exit_station , :journeys_array
+  attr_accessor :balance, :in_use, :entry_station, :exit_station, :journeys_array
 
-  MINIMUM = 1
-  LIMIT = 90
+  CARD_MINIMUM = 1
+  CARD_LIMIT = 90
+  MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
-    @in_use = false
     @entry_station = nil
     @exit_station = nil
     @journey_hash = {}
@@ -14,32 +14,35 @@ class Oystercard
   end
 
   def top_up(num)
-    raise "Card limit exceeded (£#{LIMIT})" if num + self.balance > LIMIT
+    raise "Card limit exceeded (£#{CARD_LIMIT})" if num + self.balance > CARD_LIMIT
     @balance += num
   end
 
   def touch_in(station)
-    raise "Insufficient funds - please top up" if self.balance < MINIMUM
-    @in_use = true
+    raise "Insufficient funds - please top up" if self.balance < CARD_MINIMUM
+    raise "Oystercard already in journey - please touch out" if self.in_journey? == true
     @entry_station = station
+    @exit_station = nil
     @journey_hash.clear()
     @journey_hash[:entry] = @entry_station
   end
 
   def touch_out(station)
-    @in_use = false
-    self.deduct(1)
+    raise "Oystercard not currently in journey - please touch in" if self.in_journey? == false
+    self.deduct(MINIMUM_FARE)
     @exit_station = station
+    @entry_station = nil
     @journey_hash[:exit] = @exit_station
     @journeys_array << @journey_hash
   end
 
   def in_journey?
-    @entry_station == nil ? false : true
+    !!@entry_station
   end
 
   private
+
   def deduct(amount)
-    self.balance -= amount  
+    @balance -= amount
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,15 +2,23 @@ require "oystercard"
 
 describe Oystercard do
   let(:card) { Oystercard.new }
-  let(:entry_station) { instance_double Oystercard, entry_station: "Kent House" }
-  let(:exit_station) { instance_double Oystercard, exit_station: "Victoria" }
+  let(:card_minimum) { Oystercard::CARD_MINIMUM }
+  let(:card_limit) { Oystercard::CARD_LIMIT }
+  let(:minimum_fare) { Oystercard::MINIMUM_FARE }
+  let(:entry_station_double) { instance_double Oystercard, entry_station: "Kent House" }
+  let(:exit_station_double) { instance_double Oystercard, exit_station: "Victoria" }
 
   it "has a default value of 0" do
-    expect(card.balance).to eq 0
+    expect(subject.balance).to eq 0
+  end
+
+  before do
+    card.top_up(10)
+    card.touch_in(entry_station_double)
   end
 
   it "adds money" do
-    expect { card.top_up(10) }.to change { card.balance }.to 10
+    expect { card.top_up(10) }.to change { card.balance }.to 20
   end
 
   it "throws exception when balance > 90" do
@@ -18,51 +26,45 @@ describe Oystercard do
   end
 
   it "deducts fare" do
-    card.top_up(80)
-    expect { card.send(:deduct, 10) }.to change { card.balance }.to 70
+    expect { card.send(:deduct, 1) }.to change { card.balance }.to 9
   end
 
   it "returns true when customer touches in" do
-    card.balance = Oystercard::MINIMUM
-    card.touch_in(:entry_station)
-    expect(card.in_use).to be_truthy
+    card.balance = card_minimum
+    expect(card.in_journey?).to be_truthy
   end
 
   it "returns false when customer touches out" do
-    card.touch_out(:exit_station)
-    expect(card.in_use).to be_falsy
+    card.touch_out(exit_station_double)
+    expect(card.in_journey?).to be_falsy
   end
 
   it "returns true when card.in_journey?" do
-    card.balance = Oystercard::MINIMUM
-    card.touch_in(:entry_station)
+    card.balance = card_minimum
     expect(card.in_journey?).to be_truthy
   end
 
   it "returns false when card not .in_journey?" do
-    card.touch_out(:exit_station)
+    card.touch_out(exit_station_double)
     expect(card.in_journey?).to be_falsy
   end
 
   it "raises an error if the balance is less than £1 when touching in" do
     card.balance = 0
-    expect { card.touch_in(:entry_station) }.to raise_error "Insufficient funds - please top up"
+    expect { card.touch_in(entry_station_double) }.to raise_error "Insufficient funds - please top up"
   end
 
   it "deducts money for completed journey" do
-    subject.balance = 5
-    expect { subject.touch_out(:exit_station) }.to change { subject.balance }.by(-1)
+    expect { card.touch_out(exit_station_double) }.to change { card.balance }.by(-minimum_fare)
   end
 
   it "returns entry station" do
-    card.balance = 10
-    card.touch_in(:entry_station)
-    expect(card.entry_station).to eq :entry_station
+    expect(card.entry_station).to eq entry_station_double
   end
 
   it "returns exit station" do
-    card.touch_out(:exit_station)
-    expect(card.exit_station).to eq :exit_station
+    card.touch_out(exit_station_double)
+    expect(card.exit_station).to eq exit_station_double
   end
 
   it "has an empty @journeys_array by default" do
@@ -70,10 +72,16 @@ describe Oystercard do
   end
 
   it "creates one jouney after touch_in & touch_out" do
-    card.balance = 10
-    card.touch_in(:entry_station)
-    card.touch_out(:exit_station)
+    card.touch_out(exit_station_double)
     expect(card.journeys_array.length).to eq 1
   end
 
+  it "raises an error if card is already in_journey?" do
+    expect { card.touch_in(entry_station_double) }.to raise_error "Oystercard already in journey - please touch out"
+  end
+
+  it "raises an error if card.touch_out is not currently in_journey?" do
+    card.touch_out(exit_station_double)
+    expect { card.touch_out(exit_station_double) }.to raise_error "Oystercard not currently in journey - please touch in"
+  end
 end
